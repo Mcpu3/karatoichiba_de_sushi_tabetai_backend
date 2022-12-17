6963385d5f0dff2676731e8d87021d1528bf44b4
岩本圭介 <mcpu3@outlook.jp>
2022-12-18 08:58:19 +0900

    secret.pyを追加

---
+++ twitter/secret.py
@@ -0,0 +1,13 @@
+import os
+
+
+# Account Info
+id = "pn_usakochan"
+
+# Twitter API
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+access_token = os.getenv('ACCESS_TOKEN')
+access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+bearer_token = os.getenv('BEARER_TOKEN')
