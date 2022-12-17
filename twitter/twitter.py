8df1df88fee6129b5898c49ffdcf7dbea03ac247
sny430 <shinya430.g@gmail.com>
2022-12-17 17:05:37 +0900

    :sparkles: get tweet with query

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -1,13 +1,21 @@
+from datetime import datetime,timedelta
+import time
 import tweepy
 
 import secret
 
 client = tweepy.Client(
-    consumer_key=secret.consumer_key,
-    consumer_secret=secret.consumer_secret,
-    access_token=secret.access_token,
-    access_token_secret=secret.access_token_secret
+    consumer_key = secret.consumer_key,
+    consumer_secret = secret.consumer_secret,
+    access_token = secret.access_token,
+    access_token_secret = secret.access_token_secret,
+    bearer_token = secret.bearer_token
 )
 
-text = input("Tweet?>")
-client.create_tweet(text = text)
+query = "-is:retweet to:" + secret.id
+
+tweets = client.search_recent_tweets(query = query, max_results = 10)
+
+if tweets is not None:
+    for tweet in tweets:
+        print(tweet)
