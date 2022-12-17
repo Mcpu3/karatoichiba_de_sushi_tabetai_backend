11737091f5707316016e52b1022779920ffcce78
sny430 <shinya430.g@gmail.com>
2022-12-17 23:47:04 +0900

    :truck: rename test.py -> debug.py

--- debug.py
+++ debug.py
@@ -1,6 +1,5 @@
 from pn_predictor.predict_pns import predict_pns
 
-
 texts_size = int(input('Texts size: '))
 texts = [input(f'Texts[{i}]: ') for i in range(texts_size)]
 predicted_pns = predict_pns(texts, './pn_predictor/misc/count_vectorizer.pickle', './pn_predictor/misc/model.pickle')
