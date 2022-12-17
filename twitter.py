efb5cf307fc142b317a66877c52e0497ccaf1216
sny430 <shinya430.g@gmail.com>
2022-12-17 11:48:36 +0900

    :closed_lock_with_key: refactoring token define

--- twitter.py
+++ twitter.py
@@ -1,10 +1,12 @@
 import tweepy
 
+import secret
+
 client = tweepy.Client(
-    consumer_key='',
-    consumer_secret='',
-    access_token='',
-    access_token_secret=''
+    consumer_key=secret.consumer_key,
+    consumer_secret=secret.consumer_secret,
+    access_token=secret.access_token,
+    access_token_secret=secret.access_token_secret
 )
 
 client.create_tweet(text = "Hello World")
