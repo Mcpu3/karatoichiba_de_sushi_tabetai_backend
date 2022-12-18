5419366551633f5ff5c5b1c4381774bd2d19c42e
岩本圭介 <mcpu3@outlook.jp>
2022-12-18 10:56:58 +0900

    secret.pyを修正

--- twitter/secret.py
+++ twitter/secret.py
@@ -5,9 +5,15 @@ import os
 id = "pn_usakochan"
 
 # Twitter API
-consumer_key = os.getenv('CONSUMER_KEY')
-consumer_secret = os.getenv('CONSUMER_SECRET')
-access_token = os.getenv('ACCESS_TOKEN')
-access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+# consumer_key = os.getenv('CONSUMER_KEY')
+# consumer_secret = os.getenv('CONSUMER_SECRET')
+# access_token = os.getenv('ACCESS_TOKEN')
+# access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 
-bearer_token = os.getenv('BEARER_TOKEN')
+# bearer_token = os.getenv('BEARER_TOKEN')
+consumer_key = "TsoT2tt7ySmFMWEYSzePTmAb4"
+consumer_secret = "mM95fcXxcbwvOITObJi7qoVKzNRYr0mfXUFNMhPbM5SUejCQjX"
+access_token = "1601479815868973056-z9z7wPvnUl1suJ8D66Tgwtg5ZjcKau"
+access_token_secret = "RmQoZ5dQ8XVQd3o7zL0uYBT0xvDnUluvtRuo9iDhKcJ8r"
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAN0hkQEAAAAAFQkGiTvYXabJDqzQ5ThMbvmGPqY%3DrGGpvKX1Cjc5FnWDLBAA5Ur1E0M370wDvZz3eP0W0nNoEMLsrh"
